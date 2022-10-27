19662d1ad691acf81bb187e7c70bec0f9bd7d383
s547509 <s547509@nwmissouri.edu>
2022-10-26 20:56:33 -0500

    modified app.js file

--- routes/computation.js
+++ routes/computation.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var x = Math.floor(Math.random()*10);
+    var a = Math.abs(x);
+    var b = Math.acos(x);
+    var c = Math.sin(x);
+    var d = Math.sinh(x);
+  res.render('computation', { title: 'Bonus Computation', x:x, a:a, b:b, c:c, d:d });
 });
 
 module.exports = router;
